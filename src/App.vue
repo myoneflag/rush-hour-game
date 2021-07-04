e4ef96859510f0e68bca5a2f4770da62ed6c0f04
myoneflag <arre0611@hotmail.com>
2021-07-05 01:16:33 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <select>
+      <option
+        v-for="board in boards"
+        :key="board.id"
+        :value="board.id"
+      >
+        {{ board.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      boards: [
+        {
+          id: '001',
+          name: 'Board 1',
+          dimensions: 6,
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
